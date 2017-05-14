ab77f09996822a338e598ad1a919617d7a79dd30
Platane <me@platane.me>
2017-05-14 14:37:49 +0200

    :rocket: when a filter is applied, fetch all the posts

--- src/reducer/fetcher.js
+++ src/reducer/fetcher.js
@@ -3,48 +3,55 @@ import type { State } from './index'
 
 const unique = arr => arr.filter((a, i, arr) => i === arr.indexOf(a))
 
+import { primaryTags } from './selectedTag'
+
 // manipulate the toFetch array
 export const reduceFetcher = (state: State, action: Action): State => {
     const oldFetcher = state.fetcher
     let newFetcher = state.fetcher
 
-    switch (action.type) {
-        case 'loadMorePosts': {
-            // ask to load more of a category
-            const tag = action.tag || 'all'
+    // save the next id
+    if ('postsFetched' === action.type) {
+        const { next, tag } = action
 
-            if (!state.fetcher.ended[tag]) {
-                // will load this next chunk
-                const nextId = state.fetcher.next[tag] || 'top'
+        if (tag)
+            newFetcher = {
+                ...newFetcher,
+                next: { ...newFetcher.next, [tag]: next },
+                ended: { ...newFetcher.ended, [tag]: !next },
+            }
+    }
+
+    // determine what to fetch
+    {
+        const tagToFetch = []
+
+        // explicit action
+        if ('loadMorePosts' === action.type)
+            tagToFetch.push(action.tag || 'all')
+
+        // if a filter is applied, we need to load all the data
+        if (state.selectedTag && !primaryTags.includes(state.selectedTag))
+            tagToFetch.push('all')
+
+        unique(tagToFetch)
+            .filter(tag => !newFetcher.ended[tag])
+            .forEach(tag => {
+                const nextId = newFetcher.next[tag] || 'top'
 
                 const uri = `data/${tag}/${nextId}.json`
 
                 newFetcher = {
+                    ...newFetcher,
                     toFetch: unique([uri, ...newFetcher.toFetch]),
                 }
-                break
-            }
-        }
-
-        case 'postsFetched': {
-            const { next, tag } = action
-
-            if (tag)
-                newFetcher = {
-                    next: { ...newFetcher.next, [tag]: next },
-                    ended: { ...newFetcher.ended, [tag]: !next },
-                }
-            break
-        }
+            })
     }
 
     return oldFetcher === newFetcher
         ? state
         : {
               ...state,
-              fetcher: {
-                  ...oldFetcher,
-                  ...newFetcher,
-              },
+              fetcher: newFetcher,
           }
 }
