4d1fabe8e788d790e3adad6281103bf1f920786a
Platane <me@platane.me>
2017-06-07 23:50:37 +0200

    :hammer: cache manifest url

--- scripts/buildServiceWorker.js
+++ scripts/buildServiceWorker.js
@@ -12,7 +12,7 @@ const webpackStat = JSON.parse(fs.readFileSync(webpackStatPath).toString())
 const links = {
     hostname: appPath.hostname,
     'root.html': appPath.build(''),
-    'root2.html': appPath.build('/'),
+    'root2.html': appPath.dir,
     'index.html': appPath.build('index.html'),
     'app.js': appPath.build(
         webpackStat.chunks[0].files.find(x => x.match(/\.js$/))
